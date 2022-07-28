04a0675476fb458865e179fa5358f4bcffea9c92
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-07-28 06:15:29 -0700

    Time: 12 ms (65.55%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -2,10 +2,27 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if ( s == t ) return true;
         
-        return ( s == t );
+        if ( s.size() != t.size() ) return false;
+        
+        vector < int > charCnt(27 , 0);
+        
+        for ( int i = 0 ; i < s.size() ; i++ ) {
+            
+            charCnt[s[i]-'a']++;
+            charCnt[t[i]-'a']--;
+            
+        }
+        
+        for ( int i = 0 ; i < 27 ; i++ ) {
+            
+            if ( charCnt[i] != 0 ) return false;
+            
+        }
+        
+        return true;
         
     }
+    
 };
