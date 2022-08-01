3498730d8b32089914a57b7cd794388264417bae
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-07-31 22:28:59 -0700

    Time: 4 ms (28.94%), Space: 10 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
+    
+    int backTrack(int i , int j , int m , int n , vector < vector < int > > &dp) {
         
-        vector<vector<int>> dp(m, vector<int>(n, 1));
+        if ( i >= m || j >= n ) return 0;
         
-        for ( int i = 1 ; i < m ; i++ ) {
-            
-            for ( int j = 1 ; j < n ; j++ ) {
-                
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                
-            }
-            
-        }
+        if ( i == m - 1 && j == n - 1 ) return 1;
         
-        return dp[m-1][n-1];
+        if ( dp[i][j] ) return dp[i][j];
+        
+        return dp[i][j] = backTrack(i+1,j,m,n,dp) + backTrack(i,j+1,m,n,dp);
+        
         
     }
     
+    int uniquePaths(int m, int n) {
+        
+        vector < vector < int > > dp( 101 , vector < int > (101 , 0) );
+        return backTrack(0,0,m,n,dp);
+        
+        // return vec[0][0];
+        
+    }
 };
